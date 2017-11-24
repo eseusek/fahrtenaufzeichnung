87306276b2cacd3844bfd8b85dbb0ec2a9aea0e9
eseusek <emmanuel.seusek@edu.campus02.at>
2017-11-24 18:19:31 +0100

    StartUP

---
+++ solution/app/src/test/java/at/manu_philfh_campus02/manu_phil_fahrtenaufzeichnung/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package at.manu_philfh_campus02.manu_phil_fahrtenaufzeichnung;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
